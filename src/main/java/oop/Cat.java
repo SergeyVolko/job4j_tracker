c9e90a983bf2e94386dce88066b31d5f54f5dbd4
Lenovo <rusobraz@mail.ru>
2021-10-25 10:33:57 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/oop/Cat.java
+++ src/main/java/oop/Cat.java
@@ -2,15 +2,36 @@ package oop;
 
 public class Cat {
 
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+    public void show() {
+        System.out.println("Cat name: " + this.name + " Cat food:" + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Bars");
+        black.show();
     }
 }
 
