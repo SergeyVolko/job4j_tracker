3ea612900567ee95977a41b541a89bcfce9e1fc4
Lenovo <rusobraz@mail.ru>
2021-10-25 10:22:02 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387]

---
+++ src/main/java/oop/DummyDic.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class DummyDic {
+    public String engToRus(String eng) {
+        return "Неизвестное слово." + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dic = new DummyDic();
+        System.out.println(dic.engToRus("abc"));
+    }
+}
