6ccfb2b1fecfe75d1a3c4842a36590aae6403f3b
Lenovo <rusobraz@mail.ru>
2021-10-30 19:45:30 +0300

    5. Аннотация @Override [#174891]

--- src/main/java/oop/inheritance/JSONReport.java
+++ src/main/java/oop/inheritance/JSONReport.java
@@ -2,16 +2,11 @@ package oop.inheritance;
 
 public class JSONReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + " \"name\" : " + name + System.lineSeparator()
                 + " \"body\" : " + body + System.lineSeparator()
                 + "}";
-    }
-
-    public static void main(String[] args) {
-        JSONReport jsonReport = new JSONReport();
-        String text = jsonReport.generate("\"name\"", "\"body\"");
-        System.out.println(text);
     }
 }
--- src/main/java/oop/inheritance/ReportUsage.java
+++ src/main/java/oop/inheritance/ReportUsage.java
@@ -5,5 +5,8 @@ public class ReportUsage {
         TextReport report = new TextReport();
         String text = report.generate("Report's name", "Report's body");
         System.out.println(text);
+        JSONReport jsonReport = new JSONReport();
+        text = jsonReport.generate("\"name\"", "\"body\"");
+        System.out.println(text);
     }
 }
