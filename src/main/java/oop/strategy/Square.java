a1f81641d6b8d4f26106c92e2b998cc39f8bd505
Lenovo <rusobraz@mail.ru>
2021-11-04 18:33:21 +0300

    7. Шаблон проектирования - Стратегия [#786 #224330](исправлено)

--- src/main/java/oop/strategy/Square.java
+++ src/main/java/oop/strategy/Square.java
@@ -11,8 +11,4 @@ public class Square implements Shape {
                 + "---------";
 
     }
-
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
-    }
 }
