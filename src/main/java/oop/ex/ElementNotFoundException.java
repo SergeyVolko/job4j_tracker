398ce467ebb89d5065ec49526221582dacaafdbd
Lenovo <rusobraz@mail.ru>
2021-11-06 12:16:42 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/oop/ex/ElementNotFoundException.java
@@ -0,0 +1,7 @@
+package oop.ex;
+
+public class ElementNotFoundException extends Exception {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/oop/ex/FindEl.java
@@ -0,0 +1,24 @@
+package oop.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key != null && key.equals(value[i])) {
+                rsl = i;
+            }
+        }
+        if (rsl < 0) {
+            throw new ElementNotFoundException("Element " + key + " not found.");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[] {"aaa", "bbb", "ccc"}, "fff");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
