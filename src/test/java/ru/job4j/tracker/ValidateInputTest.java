32fc2ece04f622cbc43124331b94df83944a9ed6
Lenovo <rusobraz@mail.ru>
2021-11-08 11:07:09 +0300

    2.1. Тест на ValidateInput [#311887 #225755](исправлено)

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
 
 public class ValidateInputTest {
@@ -37,21 +36,22 @@ public class ValidateInputTest {
                 new String[] {"1", "2", "3"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        int[] expected = new int[] {1, 2, 3};
-        int[] result = new int[3];
-        for (int i = 0; i < 3; i++) {
-            result[i] = input.askInt("Enter menu:");
-        }
-        assertArrayEquals(expected, result);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(2));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(3));
     }
 
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    @Test
     public void whenInvalidNotInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"-3"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        input.askInt("Enter menu:");
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-3));
     }
 }
