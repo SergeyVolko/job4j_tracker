d442e5fdbc65cb82386119235c037b162e1987df
Lenovo <rusobraz@mail.ru>
2021-11-08 11:29:16 +0300

    1. Singleton [#94606 #225886](исправлено)

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,26 +1,17 @@
 package ru.job4j.tracker;
 
 public final class SingleTracker {
-    private static Tracker tracker = new Tracker();
+    private static Tracker tracker = null;
 
     private SingleTracker() {
 
     }
 
     public static Tracker getInstance() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
         return tracker;
-    }
-
-    public Item add(Item item) {
-        return tracker.add(item);
-    }
-
-    public Item findById(int id) {
-        return tracker.findById(id);
-    }
-
-    public Item[] findAll() {
-        return tracker.findAll();
     }
 
     public Item[] findByName(String key) {
