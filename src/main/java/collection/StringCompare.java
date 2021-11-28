1cc8ded99a8650519799075af6998df378104698
Lenovo <rusobraz@mail.ru>
2021-11-28 15:10:21 +0300

    3. Компаратор для строк [#35008 #233451](исправлено)

--- src/main/java/collection/StringCompare.java
+++ src/main/java/collection/StringCompare.java
@@ -6,8 +6,7 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
         int length = Math.min(o1.length(), o2.length());
-        int rsl = 0;
-        for (int i = 0; i < length && rsl == 0; i++) {
+        for (int i = 0; i < length; i++) {
             if (o1.charAt(i) != o2.charAt(i)) {
                 return Character.compare(o1.charAt(i), o2.charAt(i));
             }
