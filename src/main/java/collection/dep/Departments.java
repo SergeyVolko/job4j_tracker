f23aa9aacc6a701ceafdaa99b844cb6528afce1a
Lenovo <rusobraz@mail.ru>
2021-11-29 22:09:28 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/collection/dep/Departments.java
@@ -0,0 +1,26 @@
+package collection.dep;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                start += el;
+                tmp.add(start);
+                start += "/";
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+    }
+}
