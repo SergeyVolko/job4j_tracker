c4a7fd976ca5d81f105abcc770b2936ba3fd5e44
Lenovo <rusobraz@mail.ru>
2021-12-11 16:23:24 +0300

    2.3. Ленивая загрузка [#249211]

---
+++ src/main/java/lambda/task6/LazyLoad.java
@@ -0,0 +1,18 @@
+package lambda.task6;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LazyLoad {
+
+    public static void main(String[] args) {
+        String[] names = {
+                "Ivan",
+        };
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("execute comparator");
+            return left.length() - right.length();
+        };
+        Arrays.sort(names, lengthCmp);
+    }
+}
