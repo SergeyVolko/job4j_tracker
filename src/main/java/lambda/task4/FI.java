603db28825add08d4e4f478136eeedb3869427e8
Lenovo <rusobraz@mail.ru>
2021-12-11 15:54:06 +0300

    2.1. Лямбда [#58165]

---
+++ src/main/java/lambda/task4/FI.java
@@ -0,0 +1,28 @@
+package lambda.task4;
+
+import lambda.task1.Attachment;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23)
+        };
+        Comparator<Attachment> comparator = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return left.getSize() - right.getSize();
+            }
+        };
+        Arrays.sort(atts, comparator);
+        /**
+         * Решение описать текстом.
+         */
+        Comparator<String> stringComparatorNatural = (left, right) -> left.compareTo(right);
+        Comparator<String> stringComparatorRevers = (left, right) -> right.compareTo(left);
+    }
+}
+
