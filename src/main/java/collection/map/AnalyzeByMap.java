cf2519e3851f6eab9dbf09ae77db4fe55e749004
Lenovo <rusobraz@mail.ru>
2022-07-25 13:37:15 +0300

    4. Аттестация [#504878](исправлено)

--- src/main/java/collection/map/AnalyzeByMap.java
+++ src/main/java/collection/map/AnalyzeByMap.java
@@ -1,65 +1,96 @@
 package collection.map;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
 
 public class AnalyzeByMap {
 
     public static double averageScore(List<Pupil> pupils) {
-        return getSubjectStream(pupils)
-                .collect(Collectors.averagingDouble(Subject::score));
+        int count = 0;
+        double sum = 0;
+        for (Pupil pupil : pupils) {
+            List<Subject> subjects = pupil.subjects();
+            count += subjects.size();
+            for (Subject subject : subjects) {
+                sum += subject.score();
+            }
+        }
+        return sum / count;
     }
 
     public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
-        return getStreamLabelGroupSubject(pupils, Collectors.averagingDouble(Subject::score))
-                .collect(Collectors.toList());
-
+        Map<String, List<Subject>> map = getMapSubjectsName(pupils);
+        List<Label> labels = new ArrayList<>();
+        int sum;
+        for (Map.Entry<String, List<Subject>> entry : map.entrySet()) {
+            sum = sumScoreSubjects(entry.getValue());
+            labels.add(new Label(entry.getKey(), (double) sum / entry.getValue().size()));
+        }
+        return labels;
     }
 
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
-        Function<Pupil, Label> function = p ->  new Label(p.name(), p.subjects().stream()
-                .mapToDouble(Subject::score)
-                .average()
-                .orElse(0));
-        return  getStreamLabelPupil(pupils, function)
-                .collect(Collectors.toList());
+        List<Label> labels = new ArrayList<>();
+        List<Subject> subjects;
+        for (Pupil pupil : pupils) {
+            subjects = pupil.subjects();
+            labels.add(new Label(pupil.name(),
+                    (double) sumScoreSubjects(subjects) / subjects.size()));
+        }
+        return  labels;
     }
 
     public static Label bestStudent(List<Pupil> pupils) {
-        Function<Pupil, Label> function = p -> new Label(p.name(), p.subjects().stream()
-                .mapToDouble(Subject::score)
-                .sum());
-        return getStreamLabelPupil(pupils, function).max(Comparator.comparing(Label::score))
-                .orElse(null);
+        int max = -1;
+        int sum;
+        Label result = null;
+        for (Pupil pupil : pupils) {
+            sum = sumScoreSubjects(pupil.subjects());
+            if (max < sum) {
+                result = new Label(pupil.name(), sum);
+                max = sum;
+            }
+        }
+        return result;
     }
 
     public static Label bestSubject(List<Pupil> pupils) {
-        return getStreamLabelGroupSubject(pupils, Collectors.summingDouble(Subject::score))
-                .max(Comparator.comparing(Label::score))
-                .orElse(null);
+        Label result = null;
+        Map<String, List<Subject>> map = getMapSubjectsName(pupils);
+        int max = -1;
+        int sum;
+        for (Map.Entry<String, List<Subject>> entry : map.entrySet()) {
+            sum = sumScoreSubjects(entry.getValue());
+            if (max < sum) {
+                result = new Label(entry.getKey(), sum);
+                max = sum;
+            }
+        }
+        return result;
     }
 
-    private static Stream<Subject> getSubjectStream(List<Pupil> pupils) {
-        return pupils.stream().flatMap(p -> p.subjects().stream());
+    private static Map<String, List<Subject>> getMapSubjectsName(List<Pupil> pupils) {
+        Map<String, List<Subject>> map = new HashMap<>();
+        String name;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                name = subject.name();
+                if (map.containsKey(name)) {
+                    map.get(name).add(subject);
+                } else {
+                    List<Subject> list = new ArrayList<>();
+                    list.add(subject);
+                    map.put(name, list);
+                }
+            }
+        }
+        return map;
     }
 
-    private static Stream<Label> getStreamLabelGroupSubject(List<Pupil> pupils,
-                                                            Collector<Subject, ?, Double>
-                                                                    collectorForGroup) {
-        return getSubjectStream(pupils)
-                .collect(Collectors.groupingBy(Subject::name, collectorForGroup))
-                .entrySet()
-                .stream()
-                .map(e -> new Label(e.getKey(), e.getValue()));
-    }
-
-    private static Stream<Label> getStreamLabelPupil(List<Pupil> pupils,
-                                                     Function<Pupil, Label> labelFunction) {
-        return pupils.stream()
-                .map(labelFunction);
+    private static int sumScoreSubjects(List<Subject> subjects) {
+        int sum = 0;
+        for (Subject subject : subjects) {
+            sum += subject.score();
+        }
+        return sum;
     }
 }
